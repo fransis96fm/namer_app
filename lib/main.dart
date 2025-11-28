c5c3fc9612a7f80b04e24589d9a8151fbeae43c6
Fransis96 <fransiskus.agustinus@students.utdi.ac.id>
2025-11-28 18:16:38 +0700

    modif class MyAppState

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,20 @@ class MyApp extends StatelessWidget {
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
 
-  // ↓ Add this.
   void getNext() {
     current = WordPair.random();
+    notifyListeners();
+  }
+  
+  // ↓ Add the code below.
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
     notifyListeners();
   }
 }
